5fb023520778f685f96d10538c47685995b8d12c
horie1024 <ryosuke.pic@gmail.com>
2018-02-15 03:45:40 +0900

    finish 「Adding a database resource」

--- handler.js
+++ handler.js
@@ -1,34 +1,76 @@
-'use strict';
+const AWS = require('aws-sdk');
 
-module.exports.helloWorld = (event, context, callback) => {
-  const response = {
+const NAMES_TABLE = process.env.NAMES_TABLE;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.saveName = (event, context, callback) => {
+  let response = {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-    },
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      'Access-Control-Allow-Origin': '*',
+    }
   };
+  const name = event.queryStringParameters.name;
+  const age = event.queryStringParameters.age;
+  console.log(`Request to save name ${name} with age ${age}`);
 
-  callback(null, response);
-};
-
-module.exports.helloName = (event, context, callback) => {
-  let name = 'stranger';
-  if (event.queryStringParameters && event.queryStringParameters.name) {
-    name = event.queryStringParameters.name;
+  const params = {
+    TableName: NAMES_TABLE,
+    Item: {
+      name,
+      age
+    },
   }
-  const response = {
+
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.log(error);
+      response.statusCode = 400;
+      response.body = JSON.stringify({ error: "Could not save name" })
+
+      callback(null, response);
+    }
+    response.body = JSON.stringify({ name, age })
+    callback(null, response);
+  });
+}
+
+module.exports.getName = (event, context, callback) => {
+  let response = {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-    },
-    body: JSON.stringify({
-      message: `Hello, ${name}!`
-    }),
+      'Access-Control-Allow-Origin': '*',
+    }
   };
 
-  callback(null, response);
+  const name = event.queryStringParameters.name;
+  console.log(`Request to retrieve name ${name}`);
+
+  const params = {
+    TableName: NAMES_TABLE,
+    Key: {
+      name
+    },
+  }
+
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      response.statusCode = 400;
+      response.body = JSON.stringify({ error: "Could not retrieve name" })
+
+      callback(null, response);
+    }
+    if (result.Item) {
+      const {name, age} = result.Item;
+      response.body = JSON.stringify({ name, age })
+
+      callback(null, response);
+    } else {
+      response.statusCode = 400;
+      response.body = JSON.stringify({ error: "Name does not exist" })
+
+      callback(null, response);
+    }
+  });
 }
